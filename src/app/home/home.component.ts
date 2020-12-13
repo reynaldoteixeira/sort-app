30808f1de1be80e49c4f2f1b0e4aeceff7f9d7d9
Reynaldo Teixeira Jr <reynaldoteixeira@gmail.com>
2020-12-13 13:05:03 -0300

    Take names values and add guide comments

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,11 +24,17 @@ export class HomeComponent implements OnInit {
         const res = reader.result as string; // This variable contains your file as text
         const lines = res.split('\n'); // Splits you file into lines
         const ids = [];
+        const names = [];
 
         lines.forEach((line) => {
           ids.push(line.split(',')[0]); // Get first item of line
         });
+
+        lines.forEach((line) => { // para cada linha
+          names.push(line.split(',')[1]); // pega oo valor da segunda coluna e armazena na variável 'names'
+        });
         console.log(ids);
+        console.log(names);
       };
     }
   }
