d1af7fece64c41cbcb5fd38f345d58cb890f1270
Reynaldo Teixeira Jr <reynaldoteixeira@gmail.com>
2020-12-13 12:59:46 -0300

    Read .csv file and first column

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,25 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  fileUpload(files){
+    if (files && files.length > 0) {
+
+      const file: File = files.item(0);
+      const reader: FileReader = new FileReader();
+      reader.readAsText(file);
+
+      reader.onload = (e) => {
+
+        const res = reader.result as string; // This variable contains your file as text
+        const lines = res.split('\n'); // Splits you file into lines
+        const ids = [];
+
+        lines.forEach((line) => {
+          ids.push(line.split(',')[0]); // Get first item of line
+        });
+        console.log(ids);
+      };
+    }
+  }
+
 }
