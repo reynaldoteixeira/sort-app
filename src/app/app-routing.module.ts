ba828c67f26a6048b3754b7ff1a8c9de3b454e3e
Reynaldo Teixeira Jr <reynaldoteixeira@gmail.com>
2020-12-14 21:43:26 -0300

    Home page layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { HtmlParser } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { UploadComponent } from './upload/upload.component';
 
 const routes: Routes = [
-  { path:'', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'upload_file', component: UploadComponent },
+  { path: 'list', component: ListComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { UploadComponent } from './upload/upload.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    UploadComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
